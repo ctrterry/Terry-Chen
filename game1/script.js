c5f95f1ae0f981beb4b0a59c3f49bdd0202a1f02
ctrterry <62485713+ctrterry@users.noreply.github.com>
2024-05-02 16:08:32 -0700

    Update script.js

--- game1/script.js
+++ game1/script.js
@@ -1,19 +1,21 @@
-
+// Retrieves elements for each door and start button
 const doorImage1 = document.getElementById('door1');
 const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
+const startButton = document.getElementById('start');
+
+// Defines image paths for doors and closed door
 const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 const closedDoorPath = "https://content.codecademy.com/projects/chore-door/images/closed_door.svg";
 
-const startButton = document.getElementById('start');
+// Initializes game state variables
 let currentPlaying = true;
-
-
 let numClosedDoors = 3;
 let openDoor1, openDoor2, openDoor3;
 
+// Checks if a door contains the bot
 const isBot = (door) => {
     if (door.src === botDoorPath) {
         return true;
@@ -22,6 +24,7 @@ const isBot = (door) => {
     }
 }
 
+// Checks if a door has been clicked
 const isClicked = (door) => {
     if (door.src === closedDoorPath) {
         return false;
@@ -30,6 +33,7 @@ const isClicked = (door) => {
     }
 }
 
+// Handles door click event and updates game state
 const playDoor = (door) => {
     --numClosedDoors;
     if (numClosedDoors === 0) {
@@ -39,28 +43,32 @@ const playDoor = (door) => {
     }
 }
 
+// Generates random chore door arrangement
 const randomChoreDoorGenerator = () => {
     let choreDoor = Math.floor(Math.random() * numClosedDoors);
 
     switch (choreDoor) {
-        case 0: openDoor1 = botDoorPath;
+        case 0: 
+            openDoor1 = botDoorPath;
             openDoor2 = beachDoorPath;
             openDoor3 = spaceDoorPath;
             break;
-        case 1: openDoor1 = beachDoorPath;
+        case 1: 
+            openDoor1 = beachDoorPath;
             openDoor2 = botDoorPath;
             openDoor3 = spaceDoorPath;
             break;
-        case 2: openDoor1 = beachDoorPath;
+        case 2: 
+            openDoor1 = beachDoorPath;
             openDoor2 = spaceDoorPath;
             openDoor3 = botDoorPath;
             break;
-        default: console.log('this is not possible');
+        default: 
+            console.log('this is not possible');
     }
-
 }
 
-
+// Handles click event for door1
 doorImage1.onclick = () => {
     if (!isClicked(doorImage1) && currentPlaying) {
         doorImage1.src = openDoor1;
@@ -68,6 +76,7 @@ doorImage1.onclick = () => {
     }
 }
 
+// Handles click event for door2
 doorImage2.onclick = () => {
     if (!isClicked(doorImage2) && currentPlaying) {
         doorImage2.src = openDoor2;
@@ -75,6 +84,7 @@ doorImage2.onclick = () => {
     }
 }
 
+// Handles click event for door3
 doorImage3.onclick = () => {
     if (!isClicked(doorImage3) && currentPlaying) {
         doorImage3.src = openDoor3;
@@ -82,13 +92,14 @@ doorImage3.onclick = () => {
     }
 }
 
+// Handles click event for start button
 startButton.onclick = () => {
     if(!currentPlaying) {
         startRound();
     }
-    
 }
 
+// Resets game state and starts a new round
 const startRound = () => {
     doorImage1.src = closedDoorPath;
     doorImage2.src = closedDoorPath;
@@ -100,6 +111,7 @@ const startRound = () => {
     randomChoreDoorGenerator();
 }
 
+// Ends the game and updates game over message
 const gameOver = (status) => {
     if (status === 'win') {
         startButton.innerHTML = "You win! Play again?";
@@ -110,4 +122,5 @@ const gameOver = (status) => {
     currentPlaying = false;
 }
 
+// Starts the first round of the game
 startRound();
